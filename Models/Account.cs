7f079106a7951bb4a7e7594456a5498808402745
Arrington Mata <mataarrington@gmailcom>
2023-08-15 14:27:09 -0600

    project initialized

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+namespace bcw_2023summer_gregslist_csharp.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
